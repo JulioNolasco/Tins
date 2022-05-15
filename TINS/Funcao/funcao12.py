3d67db15badcafda3b08c6dd68a831f2919f36bd
Julio Cezar Nolasco <47585001+JulioNolasco@users.noreply.github.com>
2022-05-15 05:49:09 -0300

    Delete TINS directory

--- TINS/Funcao/funcao12.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def embaralhar(palavra):
-    return random.sample(palavra, len(palavra)) 
-
-palavra = input("Entre com uma palavra: ")
-print(palavra, "->", embaralhar(palavra))
--- TINS/Funcao/funcao9.py
+++
@@ -1,5 +0,0 @@
-def reverso (numero):
-    return numero[::-1]
-
-numero = input("Entre com o numero: ")
-print(numero, "->", reverso(numero))
